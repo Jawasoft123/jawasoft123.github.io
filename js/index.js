d5c136e0b8d866b49a2f4e97ea24a32b8cefa506
Jaime-Santana <jaime_12_03@hotmail.com>
2021-01-18 15:25:49 -0500

    Se agrega console para imprimir el path

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	// get current URL path and assign 'active' class	
 	var pathname = window.location.pathname;
+	console.log(pathname);
 	$('ul > li > a[href="'+pathname+'"]').parent().addClass('active');
 })

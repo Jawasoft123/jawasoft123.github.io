2a3c05fe9d2a2bb5349a31c2bd06373315a81ef2
Jaime-Santana <jaime_12_03@hotmail.com>
2021-01-18 18:51:23 -0500

    Se agregan directorio y archivos de licencias, se agrega funcion y listeners para cargar licencias en productos

--- js/productos.js
+++ js/productos.js
@@ -1,23 +1,42 @@
 const jawaCheck = document.getElementById('jawaCheck');
+const buttonDownloadJawa = document.getElementById('buttonDownloadJawa');
+const buttonDownloadGenerator = document.getElementById('buttonDownloadGenerator');
 
 jawaCheck.addEventListener('change', function() {
 	if (this.checked) {		
 		document.getElementById('buttonJawaWindows').disabled = false;
-		document.getElementById('buttonJawaLinux').disabled = false;
-		loadFileAndPrintToConsole('https://jawasoft123.github.io/files/LICENSE.txt');
+		document.getElementById('buttonJawaLinux').disabled = false;		
 	} else {		
 		document.getElementById('buttonJawaWindows').disabled = true;
 		document.getElementById('buttonJawaLinux').disabled = true;
 	}
 });
 
+buttonDownloadJawa.addEventListener('click', function(){
+	var licenseJawa = loadFileAndPrintToConsole('https://jawasoft123.github.io/files/jawaLicense.txt');
+	licenseJawa.then(value=>{
+		document.getElementById('modalJawa').textContent = value;
+	})
+});
+
+buttonDownloadGenerator.addEventListener('click', function(){
+	var licenseJawa = loadFileAndPrintToConsole('https://jawasoft123.github.io/files/generatorLicense.txt');
+	licenseJawa.then(value=>{
+		document.getElementById('modalJawa').textContent = value;
+	})
+});
+
 async function loadFileAndPrintToConsole(url) {
 	try {
 		const response = await fetch(url);
-		const data = await response.text();
-		console.log(data);
+		const data = await response.text();		
+		return data;
 	} catch (err) {
 		console.error(err);
 	}
 }
+
+
+
+
 

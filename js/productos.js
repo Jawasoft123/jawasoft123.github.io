10cab8d2571eda4e0e89de0925efaa79109a285a
Jaime-Santana <jaime_12_03@hotmail.com>
2021-01-18 18:14:57 -0500

    Se agrega responsive a imagenes autores, se agrega funcion de cargar textos de ficheros a consola en productos

--- js/productos.js
+++ js/productos.js
@@ -4,8 +4,20 @@ jawaCheck.addEventListener('change', function() {
 	if (this.checked) {		
 		document.getElementById('buttonJawaWindows').disabled = false;
 		document.getElementById('buttonJawaLinux').disabled = false;
+		loadFileAndPrintToConsole('https://jawasoft123.github.io/files/LICENSE.txt');
 	} else {		
 		document.getElementById('buttonJawaWindows').disabled = true;
 		document.getElementById('buttonJawaLinux').disabled = true;
 	}
 });
+
+async function loadFileAndPrintToConsole(url) {
+	try {
+		const response = await fetch(url);
+		const data = await response.text();
+		console.log(data);
+	} catch (err) {
+		console.error(err);
+	}
+}
+
